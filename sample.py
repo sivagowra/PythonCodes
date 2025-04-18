0cdeef068fde75b562abec03247586d92efe9799
Gowra Sivannarayana <sivannarayana210@gmail.com>
2025-04-18 21:06:46 +0530

    add code

---
+++ sample.py
@@ -0,0 +1,23 @@
+import sys
+print(sys.version) #3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)]
+
+
+if 5<2:
+    print("is not good")
+else:
+    print("is Good")    # is Good
+
+name="siva"
+print(type(name))  # <class 'str'>
+print(len(name))    #4
+
+x,y,z="siva","raju","gopi"
+print(x) #siva
+print(y) #raju
+print(z) #gopi
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
